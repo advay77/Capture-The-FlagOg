fea4a4fd4b7370bc3a6082b93bae1279289ebf27
Advay Anand <advayanand37@gmail.com>
2025-03-16 00:10:32 +0530

    Update script.js
    this is for you buddy.

--- script.js
+++ script.js
@@ -681,3 +681,15 @@ function animate() {
 }
 
 animate();
+
+
+
+// div
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
